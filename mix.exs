711a80c2c9b04817d272cf46baca843ce64d157c
Guilherme Sehn <guilherme.sehn@therealreal.com>
2023-05-31 10:11:31 -0300

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -24,8 +24,6 @@ defmodule ConnGRPC.MixProject do
   defp deps do
     [
       {:grpc, "~> 0.5.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 
