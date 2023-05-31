3f4328745de59dfbca8c6120f42bb64b25e54c71
Guilherme Sehn <ghsehn@gmail.com>
2023-05-31 14:55:32 -0300

    Add tests for ConnGRPC.Pool (#3)
    Co-authored-by: Pablo Margreff <pmargreff@gmail.com>

--- test/support/fake_grpc.ex
+++ test/support/fake_grpc.ex
@@ -1,10 +1,10 @@
 defmodule FakeGRPC do
   defmodule Channel do
-    defstruct []
+    defstruct [ref: nil]
   end
 
   defmodule SuccessStub do
-    def connect(_address, _opts), do: {:ok, %FakeGRPC.Channel{}}
+    def connect(_address, _opts), do: {:ok, %FakeGRPC.Channel{ref: make_ref()}}
   end
 
   defmodule ErrorStub do
