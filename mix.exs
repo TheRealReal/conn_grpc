be61768fe9b3e3ba65cbb8813fc913c7953a3d1a
Guilherme Sehn <ghsehn@gmail.com>
2023-06-01 19:01:41 -0300

    Update readme, documentation and license files (#2)

--- mix.exs
+++ mix.exs
@@ -1,34 +1,64 @@
 defmodule ConnGRPC.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/TheRealReal/conn_grpc"
+  @version "0.1.0"
+
   def project do
     [
       app: :conn_grpc,
-      version: "0.1.0",
+      version: @version,
+      name: "ConnGRPC",
+      description: "Persistent channels, and channel pools for gRPC Elixir",
       elixir: "~> 1.10",
-      start_permanent: Mix.env() == :prod,
       deps: deps(),
+      docs: docs(),
+      package: package(),
       elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {ConnGRPC.Application, []}
+      extra_applications: extra_applications(Mix.env())
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      {:grpc, "~> 0.5.0"},
-      {:backoff, "~> 1.1"}
-    ]
-  end
-
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp extra_applications(:test), do: extra_applications(:default) ++ [:logger]
+  defp extra_applications(_), do: []
+
+  defp deps do
+    [
+      {:backoff, "~> 1.1"},
+      {:grpc, "~> 0.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["TheRealReal"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/conn_grpc",
+      source_url: @source_url,
+      main: "readme",
+      extras: [
+        "README.md",
+        "CHANGELOG.md": [filename: "changelog", title: "Changelog"],
+        "CODE_OF_CONDUCT.md": [filename: "code_of_conduct", title: "Code of Conduct"],
+        LICENSE: [filename: "license", title: "License"],
+        NOTICE: [filename: "notice", title: "Notice"]
+      ]
+    ]
+  end
 end
