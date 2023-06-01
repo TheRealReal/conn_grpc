4ed1165986b1ad57638e4aac0affa790d5ff546f
Guilherme Sehn <guilherme.sehn@therealreal.com>
2023-06-01 11:00:43 -0300

    add channel retry and backoff tests

--- test/support/fake_grpc.ex
+++ test/support/fake_grpc.ex
@@ -1,6 +1,6 @@
 defmodule FakeGRPC do
   defmodule Channel do
-    defstruct [ref: nil]
+    defstruct ref: nil
   end
 
   defmodule SuccessStub do
@@ -9,5 +9,21 @@ defmodule FakeGRPC do
 
   defmodule ErrorStub do
     def connect(_address, _opts), do: {:error, "reason"}
+  end
+
+  defmodule StatefulStub do
+    def init(opts), do: Agent.start_link(fn -> %{success: opts[:success]} end, name: __MODULE__)
+
+    def set_success(success), do: Agent.update(__MODULE__, fn _ -> %{success: success} end)
+
+    def connect(_address, _opts) do
+      %{success: success} = Agent.get(__MODULE__, & &1)
+
+      if success do
+        {:ok, %FakeGRPC.Channel{ref: make_ref()}}
+      else
+        {:error, "reason"}
+      end
+    end
   end
 end
