f574cf5cc64b80b93a1e05d19523d802b5c7bc6b
Guilherme Sehn <guilherme.sehn@therealreal.com>
2023-06-01 11:11:50 -0300

    add backoff behaviour doc

--- lib/conn_grpc/backoff.ex
+++ lib/conn_grpc/backoff.ex
@@ -1,7 +1,15 @@
 defmodule ConnGRPC.Backoff do
-  @moduledoc false
+  @moduledoc "Behaviour for implementing custom backoff"
 
+  @doc "Initializes the backoff state. This is called when the channel process is started."
   @callback new(opts :: any) :: (state :: any)
-  @callback backoff(state :: any) :: {non_neg_integer, state :: any}
+
+  @doc """
+  Generate backoff delay and new state.
+  This is called each time that we fail to connect.
+  """
+  @callback backoff(state :: any) :: {delay :: non_neg_integer, state :: any}
+
+  @doc "Reset backoff state. This is called when connecting succeeds."
   @callback reset(state :: any) :: (state :: any)
 end
